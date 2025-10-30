976c6d7ae69a3ab860d1e9d53dab3dad17027e90
EROQIN <1107891366@qq.com>
2025-10-30 08:11:54 +0800

    初始化

---
+++ src/main/java/com/erokin/campusclubmanagement/CampusClubManagementApplication.java
@@ -0,0 +1,13 @@
+package com.erokin.campusclubmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusClubManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampusClubManagementApplication.class, args);
+    }
+
+}
